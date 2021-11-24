8d73f3894b75b191c531842f8d9b30dbfddd576f
Mahmoud Ghonim <64014551+eng-MahmoudGhonim@users.noreply.github.com>
2021-11-24 19:29:04 +0400

    All localization done

--- aboutRTA/about RTA/lib/aboutRTA.js
+++ aboutRTA/about RTA/lib/aboutRTA.js
@@ -14,13 +14,128 @@ var data={
       fire:"Fire Department"
     },
     ar:{
-
+      headerAbout:"مرحبًا، اسمي سهيل!",
+      contentAbout:"<span class='text'>رفيق التنقل الدائم، والذي يساعدك على تحقيق أفضل تجربة تنقل في دبي</span>"+
+    "<span class='text'>.يمكنك سهيل من التخطيط لرحلاتك وحجز مركبات الأجرة وتزويدك بمقارنات الأسعار، شحن بطاقة نول الخاصة بك، وتوفير رؤية شاملة على وسائل التنقل المتاحة للقيام برحلاتك</span>"+
+    "<span class='text'>.يتوفر التطبيق بين يديك ويضمن أن تكون رحلتك سلسة وممتعة </span>",
+      ratingHeader:"تقييم وملاحظات المتعاملين",
+       rating:".تأكّد من تزويدنا بتعليقاتك من خلال تقييم التطبيق ومؤشر السعادة لنتمكن من معرفة مدى رضاك عليه ",
+      versionHeader:"إصدار تطبيق سهيل",
+      versionContent:"<span class='detailsContent'>أنت تستخدم الآن الإصدار  3.8.3(227))</span><span>تاريخ الإصدار 2021-11-15</span>",
+      socailHeader:"هيئة الطرق والمواصلات على وسائل التواصل الاجتماعي",
+      emergencyHeader:"أرقام الطوارئ",
+      police:"الشرطة والإسعاف",
+      fire:"الإدارة العامة للدفاع المدني"
+    },
+    fr:{//france
+      headerAbout:"Bonjour, c'est S'Hail! ",
+      contentAbout:"<span class='text'>Je suis votre compagnon de voyage personnel et je peux vous aider à optimiser votre expérience de voyage à Dubaï. </span>"+
+    "<span class='text'>Je peux planifier vos voyages, vous fournir des comparaisons de prix, des réservations de vos taxis, complotez votre carte NOL et vous fournir de nombreuses informations de voyage essentielles. </span>"+
+    "<span class='text'>Prêt à vos doigts, je vais vous guider dans un voyage sûr et joyeux. </span>",
+      ratingHeader:"Notes client et commentaires. ",
+       rating:"S'il vous plaît, donnez-nous vos commentaires via la notation de bonheur et dans les magasins afin que nous comprenons à quel point vous valorisez l'application. ",
+      versionHeader:"Version d'application S'Hail ",
+      versionContent:"<span class='detailsContent'>Actuellement, vous utilisez  3.8.3 (227)</span><span>Date de sortie 15-11-2021</span>",
+      socailHeader:"RTA sur les médias sociaux ",
+      emergencyHeader:"Numéros d'urgence ",
+      police:"Police et ambulance ",
+      fire:"Pompiers"
+    },
+    de:{//german
+      headerAbout:"Hallo, das ist s'hail! ",
+      contentAbout:"<span class='text'>Ich bin Ihr persönlicher Reisebegleiter und ich kann Ihnen helfen, Ihre Reiseerlebnisse in Dubai zu maximieren. </span>"+
+    "<span class='text'>Ich kann Ihre Reisen planen, bieten Ihnen Preisvergleiche, Buchungen Ihr Taxis, top Ihre NOL-Karte und bieten Ihnen viele wesentliche Reiseinformationen an. </span>"+
+    "<span class='text'>Bereit an Ihren Fingerspitzen, werde ich Sie durch eine sichere und freudige Reise führen. </span>",
+      ratingHeader:"Kundenbewertungen und Feedback. ",
+       rating:"Bitte geben Sie uns Ihr Feedback durch Glücksbewertung und durch Geschäfte, damit wir verstehen, wie viel Sie die App schätzen. ",
+      versionHeader:"S'Hail-Anwendungsversion. ",
+      versionContent:"<span class='detailsContent'>Derzeit verwenden Sie 3.8.3 (227)</span><span>Veröffentlichungsdatum  15-11-2021</span>",
+      socailHeader:"RTA auf Social Media ",
+      emergencyHeader:"Notrufnummern",
+      police:"Polizei & Ambulanz",
+      fire:"Feuerwehr"
+    },
+    es:{//spain
+      headerAbout:"Hola, esto es s'hail!",
+      contentAbout:"<span class='text'>Soy su compañero de viaje personal y puedo ayudarlo a maximizar su experiencia de viaje en Dubai.</span>"+
+    "<span class='text'>Puedo planificar sus viajes, brindarle comparaciones de precios, reservas a sus taxis, recopile su tarjeta NOL y le brinda mucha información de viaje esencial. </span>"+
+    "<span class='text'>Listo a su alcance, lo guiaré a través de un viaje seguro y alegre. </span>",
+      ratingHeader:"Calificaciones y comentarios de los clientes. ",
+       rating:"Por favor, dénos sus comentarios a través de la calificación de la felicidad y a través de las tiendas, por lo que entendemos cuánto valora la aplicación. ",
+      versionHeader:"Versión de solicitud de s'hail ",
+      versionContent:"<span class='detailsContent'>Actualmente estás utilizando  3.8.3 (227)</span><span>Fecha de lanzamiento 15-11-2021</span>",
+      socailHeader:"RTA en las redes sociales",
+      emergencyHeader:"Números de emergencia",
+      police:"Policía y ambulancia",
+      fire:"Cuerpo de Bomberos "
+    },
+    cn:{//china
+      headerAbout:"你好，这是s'hail！",
+      contentAbout:"<span class='text'>我是您的个人旅行伴侣，我可以帮助您在迪拜最大限度地致超您的旅游体验。</span>"+
+    "<span class='text'>我可以规划您的旅行，为您提供价格比较，预订您的出租车，请将您的NOL卡提高并为您提供许多必不可少的旅行信息。</span>"+
+    "<span class='text'>准备好在你的指尖，我将通过安全愉快的旅程引导您。 </span>",
+      ratingHeader:"客户评分和反馈。",
+       rating:"请通过幸福评级和商店提供您的反馈，以便我们了解您的应用程序。",
+      versionHeader:"S'Hail应用程序版本",
+      versionContent:"<span class='detailsContent'>目前您正在使用 3.8.3 (227)</span><span>发布日期 15-11-2021</span>",
+      socailHeader:"社交媒体上的RTA",
+      emergencyHeader:"紧急电话号码 ",
+      police:"警察与救护车 ",
+      fire:"消防部门 "
+    },
+    ru:{//Russia
+      headerAbout:"Здравствуйте, это S'hail! ",
+      contentAbout:"<span class='text'>Я ваш личный компаньон путешествия, и я могу помочь вам максимизировать ваш опыт путешествия в Дубае.</span>"+
+    "<span class='text'>Я могу спланировать свои поездки, предоставить вам сравнение цен, бронируя свои такси, пополните свою карту NOL и предоставить вам много важной информации о поездке.</span>"+
+    "<span class='text'>Готов к консультациям под рукой я буду направлять вас через безопасное и радостное путешествие. </span>",
+      ratingHeader:"Рейтинги клиентов и обратная связь. ",
+       rating:"Пожалуйста, дайте нам свой отзыв через рейтинг счастья и через магазины, поэтому мы понимаем, сколько вы цените приложение. ",
+      versionHeader:"В настоящее время вы",
+      versionContent:"<span class='detailsContent'>используете 3.8.3 (227)</span><span>Дата выпуска 15-11-2021</span>",
+      socailHeader:"RTA на социальных сетях",
+      emergencyHeader:"Экстренные номера ",
+      police:"Полиция и скорая помощь ",
+      fire:"Пожарная часть "
+    },
+    in:{//India
+      headerAbout:"हैलो, यह s'hail है! ",
+      contentAbout:"<span class='text'>मैं आपका निजी यात्रा साथी हूं और मैं आपको दुबई में अपने यात्रा अनुभव को अधिकतम करने में मदद कर सकता हूं।</span>"+
+    "<span class='text'>मैं आपकी यात्राओं की योजना बना सकता हूं, आपको मूल्य तुलना प्रदान करता हूं, आपकी टैक्सियों की बुकिंग, अपने एनओएल कार्ड को टॉपअप कर सकता हूं और आपको कई आवश्यक यात्रा जानकारी प्रदान करता हूं। </span>"+
+    "<span class='text'>अपनी उंगलियों पर तैयार, मैं आपको एक सुरक्षित और आनंददायक यात्रा के माध्यम से मार्गदर्शन करूंगा। </span>",
+      ratingHeader:"ग्राहक रेटिंग और प्रतिक्रिया। ",
+       rating:"कृपया हमें खुशी रेटिंग और दुकानों के माध्यम से अपनी प्रतिक्रिया दें ताकि हम समझ सकें कि आप ऐप को कितना महत्व देते हैं। ",
+      versionHeader:"S'Hail आवेदन संस्करण ",
+      versionContent:"<span class='detailsContent'>वर्तमान में आप 3.8.3 (227) का उपयोग कर रहे हैं। </span><span>रिलीज दिनांक date 15-11-2021</span>",
+      socailHeader:" सोशल मीडिया पर आरटीए ",
+      emergencyHeader:"आपातकालीन नंबर ",
+      police:"पुलिस और एम्बुलेंस ",
+      fire:"आग बुझाने का डिपो "
+    },
+    ur:{//Urdu
+      headerAbout:"ہیلو، یہ سہیل ہے۔",
+      contentAbout:"<span class='text'>میں آپ کا ذاتی سفری ساتھی ہوں اور میں دبئی میں آپ کے سفر کے تجربے کو زیادہ سے زیادہ کرنے میں آپ کی مدد کر سکتا ہوں۔</span>"+
+    "<span class='text'>میں آپ کے دوروں کی منصوبہ بندی کر سکتا ہوں، آپ کو قیمتوں کا موازنہ فراہم کر سکتا ہوں، آپ کی ٹیکسیوں کی بکنگ کر سکتا ہوں، آپ کے نول کارڈ کو ٹاپ اپ کر سکتا ہوں اور آپ کو بہت سی ضروری سفری معلومات فراہم کر سکتا ہوں۔</span>"+
+    "<span class='text'>آپ کی انگلیوں پر تیار، میں آپ کو محفوظ اور خوشگوار سفر میں رہنمائی کروں گا۔</span>",
+      ratingHeader:"کسٹمر ریٹنگز اور فیڈ بیک۔",
+       rating:"براہ کرم ہمیں خوشی کی درجہ بندی اور اسٹورز کے ذریعے اپنی رائے دیں تاکہ ہم سمجھ سکیں کہ آپ ایپ کو کتنی اہمیت دیتے ہیں۔",
+      versionHeader:"سہیل ایپلی کیشن ورژن",
+      versionContent:"<span class='detailsContent'>فی الحال آپ  استعمال کر رہے ہیں۔ 3.8.3 (227)</span><span>ریلیز کی تاریخ 15-11-2021</span>",
+      socailHeader:" سوشل میڈیا پر آر ٹی اے ",
+      emergencyHeader:"ایمرجنسی نمبر ",
+      police:"پولیس اور ایمبولینس ",
+      fire:"فائر بریگیڈ "
     }
-
 }
 headerAbout=document.getElementById("headerAbout");
 contentAbout=document.getElementById("contentAbout");
-
+ratingHeader=document.getElementById("ratingHeader");
+ratingDetails=document.getElementById("ratingDetails");
+versionHeader=document.getElementById("versionHeader");
+versionContent=document.getElementById("versionContent");
+socialHeader=document.getElementById("socialHeader");
+emergencyNumbers=document.getElementById("emergencyNumbers");
+policeNumber=document.getElementById("policeNumber");
+fireNumber=document.getElementById("fireNumber");
 function getParameterByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
@@ -30,19 +145,51 @@ function getParameterByName(name, url = window.location.href) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-
-
+function setData(data){
+  headerAbout.innerHTML=data.headerAbout;
+  contentAbout.innerHTML=data.contentAbout;
+  ratingHeader.innerHTML=data.ratingHeader;
+  ratingDetails.innerHTML=data.rating;
+  versionHeader.innerHTML=data.versionHeader;
+  versionContent.innerHTML=data.versionContent;
+  socialHeader.innerHTML=data.socailHeader;
+  emergencyNumbers.innerHTML=data.emergencyHeader;
+  policeNumber.innerHTML=data.police;
+  fireNumber.innerHTML=data.fire;
+}
 var lang=getParameterByName("lang");
 if(lang){
      switch (lang) {
-         case "en":
-            headerAbout.innerHTML=data.en.headerAbout;
-            contentAbout.innerHTML=data.en.contentAbout;
-
-             break;
-     
-         default:
-             break;
+        case "en":
+          setData(data.en);
+          break;
+        case "ar":
+          setData(data.ar);
+          break;
+        case "fr":
+          setData(data.fr);
+          break;
+        case "de":
+          setData(data.de);
+          break;
+        case "es":
+          setData(data.es);
+          break;
+        case "cn":
+          setData(data.cn);
+          break;
+        case "ru":
+          setData(data.ru);
+          break;
+        case "in":
+          setData(data.in);
+          break;
+        case "ur":
+          setData(data.ur);
+          break;
+        default:
+          setData(data.en);
+          break;
      }
 
 
